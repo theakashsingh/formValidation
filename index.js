b688f04d36516421a8f1b7657e800fdeaae6551b
theakashsingh <singhaksh@gmail.com>
2021-04-01 00:24:55 +0530

    html and css done

---
+++ index.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const cfnpassword = document.getElementById('cfnpassword');
+
+
